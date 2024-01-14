89ad9e944aeec19ad41f89862de769ce8ed566d2
nilshendriks <info@nirusu.me>
2024-01-14 14:37:58 +0100

    - updated preview branch to do ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,29 +2,30 @@ import { defineConfig } from 'astro/config';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
-
+import netlify from "@astrojs/netlify";
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_PREVIEW_TOKEN,
-      components: {
-        page: 'storyblok/Page',
-        feature: 'storyblok/Feature',
-        grid: 'storyblok/Grid',
-        teaser: 'storyblok/Teaser',
-      },
-      apiOptions: {
-        region: "eu",
-      },
-    }),
-  ],
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_PREVIEW_TOKEN,
+    components: {
+      page: 'storyblok/Page',
+      feature: 'storyblok/Feature',
+      grid: 'storyblok/Grid',
+      teaser: 'storyblok/Teaser'
+    },
+    apiOptions: {
+      region: "eu"
+    }
+  })],
   vite: {
     plugins: [basicSsl()],
     server: {
-      https: true,
-    },
+      https: true
+    }
   },
+  output: "server",
+  adapter: netlify()
 });
